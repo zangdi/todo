e99340b88d53b19fdccc2713ef7b3dda9af47c78
Zander Zhuang <zander.zhuang@gmail.com>
2020-08-31 21:41:52 +1000

    Online SQL Server

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,24 @@
 import os
+import urllib.parse
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
 # Get directory path of our file
-basedir = os.path.abspath(os.path.dirname(__file__))
+db_string = os.environ['dbstring']
+params = urllib.parse.quote_plus(db_string)
 
-# Create path to our sqlite file based on the directory path above
-app.config['SQLALCHEMY_DATABASE_URI'] =\
-    'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SECRET_KEY'] = 'supersecret'
+app.config['SQLALCHEMY_DATABASE_URI'] = "mssql+pyodbc:///?odbc_connect=%s" % params
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# basedir = os.path.abspath(os.path.dirname(__file__))
+
+#app.config['SQLALCHEMY_DATABASE_URI'] =\
+#'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+#app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 #Wrap our Flask Alchemy instance around our Flask Application
 db = SQLAlchemy(app)
